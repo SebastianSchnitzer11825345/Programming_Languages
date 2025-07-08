632e15b79dbdadcaa4a31e8b5047afefdaa62afe
SebastianSchnitzer11825345 <e11825345@student.tuwien.ac.at>
2025-07-08 13:21:23 +0200

    Implement basic layout for calculator + Implement add and subtract

---
+++ assignment_01/Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,118 @@
+package org.example;
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class Calculator {
+    private Stack<Object> stack = new Stack<>();
+
+    public Calculator(Stack<Object> stack) {
+        this.stack = stack;
+    }
+
+    public Calculator() {
+
+    }
+
+    public void push(Object o) {
+        if(o instanceof Integer || o instanceof Double || o instanceof String) {
+            stack.push(o);
+        } else {
+            throw new IllegalArgumentException("Object to push does not match expected type");
+        }
+    }
+
+    public Object pop() {
+        if(stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.pop();
+    }
+
+    public void reset() {
+        stack.clear();
+    }
+
+    public void executeCommand(char command) {
+        switch (command) {
+            case '+':
+                add();
+                break;
+            case '-':
+                subtract();
+                break;
+            case '*':
+            case '/':
+            case '%':
+            case '&':
+            case '|':
+            case '_':
+            case '?':
+            case '!':
+            case '$':
+            case '@':
+            case '\\':
+            case '#':
+            case '\'':
+            case '"':
+            case '=':
+            case '<':
+            case '>':
+            default:
+                throw new UnsupportedOperationException("Unsupported command: " + command);
+        }
+    }
+
+
+    private void add() {
+        Object a = stack.pop();
+        Object b = stack.pop();
+
+        if(a instanceof String || b instanceof String) {
+            stack.push(String.valueOf(a) + b);
+        }
+        else if(a instanceof Double || b instanceof Double) {
+            stack.push(toDouble(a) + toDouble(b));
+        }
+        else stack.push((Integer)a + (Integer) b);
+    }
+
+    private void subtract() {
+        Object a = stack.pop();
+        Object b = stack.pop();
+
+        if(a instanceof String && b instanceof Integer) {
+            if((Integer) b < 0 ) {
+                throw new IllegalArgumentException("Subtraction requires a positive number");
+            }
+            stack.push(((String) a).substring((Integer) b ));
+
+            return;
+        }
+        if(a instanceof Integer && b instanceof String) {
+            if((Integer) a < 0 ) {
+                throw new IllegalArgumentException("Subtraction requires a positive number");
+            }
+            stack.push(((String) b).substring(0, ((String) b).length()-((Integer) a)));
+            return;
+        }
+
+        if(a instanceof Double || b instanceof Double) {
+            if(a instanceof String || b instanceof String) throw new IllegalArgumentException("Cannot subtract String and Double");
+
+            stack.push(toDouble(a) - toDouble(b));
+        }
+        else stack.push((Integer)a - (Integer)b);
+    }
+
+
+    /**
+     * Simple helper function that casts both numeric Objects from the stack to double
+     * without needing to typecheck every time
+     *
+     */
+    private double toDouble(Object o) {
+        if (o instanceof Integer) return ((Integer) o).doubleValue();
+        if (o instanceof Double) return (Double) o;
+        throw new IllegalArgumentException("Received NaN in toDouble");
+    }
+}
