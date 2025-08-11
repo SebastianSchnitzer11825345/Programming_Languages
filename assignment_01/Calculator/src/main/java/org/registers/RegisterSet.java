fa784e4d1caee158f5aceb9dc5e64b821b3fa010
SebastianSchnitzer11825345 <e11825345@student.tuwien.ac.at>
2025-08-11 20:39:25 +0200

    Implement Initial Version of string analyser

--- assignment_01/Calculator/src/main/java/org/registers/RegisterSet.java
+++ assignment_01/Calculator/src/main/java/org/registers/RegisterSet.java
@@ -25,8 +25,20 @@ public class RegisterSet implements IReadOnlyRegisters {
 
         temp.put('d', reg_d_factorial());
 
+        temp.put('e', reg_e_stringAnalyser());
+
+        temp.put('f', reg_f_analyse_loop());
+
+        temp.put('g', reg_g_finalOutput());
+        temp.put('h', reg_h_classifier());
+        temp.put('i', reg_i_flushWord());
+        temp.put('j', reg_j_letter());
+        temp.put('k', reg_k_digit());
+        temp.put('l', reg_l_space());
+        temp.put('m', reg_m_special());
+
         // b–z, as a already taken
-        for (char c = 'e'; c <= 'z'; c++) {
+        for (char c = 'n'; c <= 'z'; c++) {
             temp.put(c, defaultValue(c));
         }
 
@@ -78,7 +90,7 @@ public class RegisterSet implements IReadOnlyRegisters {
     private String reg_c_if_then() {
         StringBuilder contents = new StringBuilder();
         contents.append("1 "); // starting stack (1 is true)
-        contents.append("8)(9~)(4!4$_1+$@)@");
+        contents.append("(8)(9~)(4!4$_1+$@)@");
         return contents.toString();
     }
 
@@ -93,6 +105,202 @@ public class RegisterSet implements IReadOnlyRegisters {
         A = A.replace("C",C);
         return factorial_Input.replace("A",A);
     }
+
+    private String reg_e_stringAnalyser() {
+        StringBuilder contents = new StringBuilder();
+        contents.append("(Enter string to analyse) ");
+        contents.append('"');
+        contents.append('\''); //Read input string
+        contents.append('@');
+        contents.append('0'); // starting index
+        contents.append("0 0 0 0 0"); //Counters for word, letter, digit, space, special characters
+        contents.append("0"); //currentWord (for word reversing)
+        contents.append("()"); // output string
+        contents.append('f'); // jump tp main loop
+        contents.append('@');
+
+        return contents.toString();
+    }
+
+    private String reg_f_analyse_loop() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("9!"); //copy index
+        contents.append("10!%"); //get char at index from input string
+
+
+        contents.append("()="); // check for end of string
+        contents.append("(g)@"); // if yes, go to output
+        contents.append("_"); // if no, continue
+
+        contents.append("9!"); //Copy char again
+        contents.append("(h)@"); // Call helper function, pushes category: 0=letter, 1=digit, 2=space, 3=special
+
+        contents.append("3!"); // Check if char is a letter
+        contents.append("0=");
+        contents.append("(f_j)@");
+        contents.append("_");
+
+        contents.append("3!"); //Check if char is a digit
+        contents.append("1=");
+        contents.append("(f_k)@");
+        contents.append("_");
+
+        contents.append("3!"); //Check if char is space
+        contents.append("2=");
+        contents.append("(f_l)@");
+        contents.append("_");
+
+        contents.append("(f_m)@"); // Else is special character
+
+        return contents.toString();
+    }
+
+    private String reg_g_finalOutput() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("(i)@"); //Flush last output
+
+        contents.append("10!\"");
+
+        contents.append("(Words: )7!\"");
+        contents.append("(Letters: )6!\" ");
+        contents.append("(Digits: )5!\" ");
+        contents.append("(Spaces: )4!\" ");
+        contents.append("(Special: )3!\"");
+
+        return contents.toString();
+    }
+
+    private String reg_h_classifier() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("%"); //Get Ascii code of letter
+
+        //65-90, 97-122 --> Letter
+        contents.append("65>"); //
+        contents.append("1"); // not letter
+        contents.append("97<");
+        contents.append("1|"); //
+        contents.append("0_");
+
+
+        //48-57 --> Digit
+        contents.append("48<");
+        contents.append("57>");
+        contents.append("&");
+        contents.append("1&");
+        contents.append("1_");
+
+        // 32 --> Space
+        contents.append("32=");
+        contents.append("2&");
+
+        // Otherwise Special character
+        contents.append("3|");
+
+        return contents.toString();
+    }
+
+    private String reg_i_flushWord() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("8!"); // get word
+        contents.append("()=");
+        contents.append("_"); //if word is not empty, continue
+
+        contents.append("1");
+        contents.append("7!1+");
+        contents.append("$");
+
+        contents.append("8!"); //get word
+        contents.append("~"); //reverse string
+        contents.append("10!+"); // append to output
+        contents.append("$");
+
+        contents.append("()");
+        contents.append("$");
+
+        return contents.toString();
+    }
+
+    private String reg_j_letter() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("8!"); //copy word
+        contents.append("9!+"); //append char to word
+        contents.append("$"); // replace old word
+
+        contents.append("6!1+"); // letters++
+        contents.append("$");
+
+        contents.append("2!1+"); // index++
+        contents.append("$");
+
+        contents.append("(f)@"); // loop back
+
+        return contents.toString();
+    }
+
+    private String reg_k_digit() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("8!"); //copy word
+        contents.append("9!+"); //append char to word
+        contents.append("$"); // replace old word
+
+        contents.append("5!1+"); // digit++
+        contents.append("$");
+
+        contents.append("2!1+"); // index++
+        contents.append("$");
+
+        contents.append("(f)@"); // loop back
+
+        return contents.toString();
+    }
+
+    private String reg_l_space() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("(i)@"); //flush word
+
+        contents.append("4!1+"); // space++
+        contents.append("$");
+
+        contents.append("2!1+"); // index++
+        contents.append("$");
+
+        contents.append("10!9!+"); //output + space
+        contents.append("$");
+
+        contents.append("(f)@"); // loop back
+
+        return contents.toString();
+    }
+
+    private String reg_m_special() {
+        StringBuilder contents = new StringBuilder();
+
+        contents.append("(i)@"); //flush word
+
+        contents.append("3!1+"); // special++
+        contents.append("$");
+
+        contents.append("2!1+"); // index++
+        contents.append("$");
+
+        contents.append("10!9!+"); //output + space
+        contents.append("$");
+
+        contents.append("(f)@"); // loop back
+
+        return contents.toString();
+    }
+
+
+
+
 
     private Object defaultValue(char name) {
         if (Character.isLowerCase(name)) {
