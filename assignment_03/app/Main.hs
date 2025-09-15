ca615cb385f1a5e498571b83e31053424590204d
alenka triplat <alenka.triplat@gmail.com>
2025-09-15 12:20:01 +0200

    cleaning up

--- assignment_03/app/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Lib
-
-main :: IO ()
-main = do
-  print $ topLevel1 parser1 program
-  print $ topLevel2 parser2 program
-    where program = "PROGRAM Foo2 WHILE == 1 1 DO var = 1."
